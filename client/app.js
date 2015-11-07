d4b7a84d7e16327887c79e38b109c864068edee0
Filip Klimes <filip@filipklimes.cz>
2015-11-07 21:08:32 +0100

    Added node-fetch

--- client/app.js
+++ client/app.js
@@ -1,14 +1,33 @@
+import fetch from 'node-fetch';
 import React from 'react';
 import EntityMetadataFactory from './model/EntityMetadataFactory.js';
 import EntityForm from './components/EntityForm.js';
-import {metadata} from './components/metadata.js'
+
+// TODO: parametrize the entity name
+let entityName = "cz.cvut.fel.nutforms.example.model.Bug";
+
+
+function renderComponent(metadata) {
+
+    let entityMetadata = EntityMetadataFactory.create(entityName, metadata);
+
+    React.render(
+        <EntityForm metadata={entityMetadata}/>,
+        document.getElementById('form')
+    );
+
+}
 
 // TODO: load data from API (/meta, /rule)
-
-let entityName = "cz.cvut.fel.nutforms.example.model.Bug";
-let entityMetadata = EntityMetadataFactory.create(entityName, metadata);
-
-React.render(
-    <EntityForm metadata={entityMetadata} />,
-    document.getElementById('form')
-);
+// TODO: parametrize URL of the api
+// TODO: enable CORS (now ahs to be disabled via developer mode in browser)
+fetch(`http://localhost:8080/meta/class/${entityName}`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (json) {
+        renderComponent(json);
+    })
+    .catch(function (ex) {
+        console.log('Loading entity metadata failed!', ex)
+    });
