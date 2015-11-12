6051a1950cc29a316f6044bb9ddb24ba2067b6d6
Filip Klimes <filip@filipklimes.cz>
2015-11-12 22:34:58 +0100

    Added ModelFactory

--- client/src/main/javascript/model/Model.js
+++ client/src/main/javascript/model/Model.js
@@ -11,12 +11,62 @@ export default class Model {
      * @param {string} context
      * @param {*} id
      * @param {Attribute[]} attributes
+     * @param {Relation[]} relations
      */
-    constructor(className, context, id, attributes) {
+    constructor(className, context, id, attributes, relations) {
         this.className = className;
         this.context = context;
         this.id = id;
         this.attributes = attributes;
+        this.relations = relations;
+    }
+
+    /**
+     * Checks if the model has Attribute with given name.
+     *
+     * @param {string} name
+     * @returns {boolean}
+     */
+    hasAttribute(name) {
+        return this.attributes.hasOwnProperty(name);
+    }
+
+    /**
+     * Returns Attribute with given name.
+     *
+     * @param {string} name
+     * @returns {Attribute}
+     * @throws Will throw an error if Attribute with given name does not exist.
+     */
+    getAttribute(name) {
+        if (!this.hasAttribute(name)) {
+            throw `This model does not contain Attribute with name ${name}.`;
+        }
+        return this.attributes[name];
+    }
+
+    /**
+     * Checks if the model has Relation with given name.
+     *
+     * @param {string} name
+     * @returns {boolean}
+     */
+    hasRelation(name) {
+        return this.relations.hasOwnProperty(name);
+    }
+
+    /**
+     * Returns Relation with given name.
+     *
+     * @param {string} name
+     * @returns {Relation}
+     * @throws Will throw an error if Relation with given name does not exist.
+     */
+    getRelation(name) {
+        if (!this.hasRelation(name)) {
+            throw `This model does not contain Relation with name ${name}.`;
+        }
+        return this.relations[name];
     }
 
 }
--- client/test/main/javascript/model/Model.js
+++ client/test/main/javascript/model/Model.js
@@ -4,7 +4,7 @@ import Model from '../../../../src/main/javascript/model/Model.js';
 
 describe('model.Model', function () {
     it('should have Observable trait', function () {
-        let model = new Model("cz.cvut.fel.nutforms.example.model.Bug", "context", null, []);
+        let model = new Model("cz.cvut.fel.nutforms.example.model.Bug", "context", null, {}, {});
         assert.equal('function', typeof model.listen);
     });
 });
