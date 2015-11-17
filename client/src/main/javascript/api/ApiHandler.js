b1452142f9465d489c70fd7ffdeb42c57826d1e1
Filip Klimes <filip@filipklimes.cz>
2015-11-17 13:07:12 +0100

    Now loads both metadata and data from the API and then creates a form

---
+++ client/src/main/javascript/api/ApiHandler.js
@@ -0,0 +1,87 @@
+import fetch from 'node-fetch';
+import Base64 from './Base64.js';
+
+/**
+ * Handles calls to API.
+ */
+export default class ApiHandler {
+
+    /**
+     * ApiHandler constructor.
+     *
+     * @param {string} apiAddress
+     * @param {string} apiUser
+     * @param {string} apiPassword
+     */
+    constructor(apiAddress, apiUser, apiPassword) {
+        this.apiAddress = apiAddress;
+        this.apiUser = apiUser;
+        this.apiPassword = apiPassword;
+
+        this._toJson = response => response.json();
+        this._logResponse = (message) => {
+            return response => {
+                console.log(message, response);
+                return response
+            };
+        };
+
+    }
+
+    /**
+     * Fetches metadata for given class.
+     *
+     * @param className
+     */
+    fetchMetadataFor(className) {
+
+        let classMetadataPromise = fetch(this._buildUrl('meta/class/' + className))
+            .then(this._toJson)
+            .then(this._logResponse("Class metadata loaded from API"));
+        // TODO: more metadata
+
+        return Promise.all([classMetadataPromise]);
+    }
+
+    /**
+     * Fetches data for given class with given id.
+     *
+     * @param {string} className
+     * @param {*} id
+     */
+    fetchDataFor(className, id) {
+
+        return Promise.resolve(fetch(this._buildUrl('' + className.split(".").pop() + '/' + id), {
+                headers: {
+                    Authorization: "Basic " + Base64.encode(this.apiUser + ":" + this.apiPassword)
+                    , Accept: "application/json;charset=UTF-8"
+                    , "Content-type": "application/json;charset=UTF-8"
+                }
+            }))
+            .then(this._toJson)
+            .then(this._logResponse("Entity data loaded from API"));
+    }
+
+    /**
+     * Submits given model to the API using given method.
+     *
+     * @param {string} className
+     * @param {Model} model
+     * @param {string} method
+     */
+    submit(className, model, method) {
+        // TODO: submit data
+    }
+
+    /**
+     * Builds whole URL of the resource for given path.
+     *
+     * @param {string} path
+     * @returns {string}
+     * @private
+     */
+    _buildUrl(path) {
+        return this.apiAddress + path;
+    }
+
+}
--- client/src/main/javascript/app.js
+++ client/src/main/javascript/app.js
@@ -1,59 +1,28 @@
 import fetch from 'node-fetch';
 import React from 'react';
 import Model from './model/Model.js';
+import ApiHandler from './api/ApiHandler.js';
 import EntityForm from './components/EntityForm.js';
 import ModelFactory from './model/ModelFactory.js';
 
-// TODO: load from API
 let id = 1;
 let className = "cz.cvut.fel.nutforms.example.model.Bug";
-let metadata = {
-    "attributes": [
-        {
-            "name": "description",
-            "type": "java.lang.String",
-            "is_primary": false
-        },
-        {
-            "name": "log",
-            "type": "java.lang.String",
-            "is_primary": false
-        },
-        {
-            "name": "id",
-            "type": "java.lang.Long",
-            "is_primary": true
-        },
-        {
-            "name": "localizedDescription",
-            "type": "java.lang.String",
-            "is_primary": false
-        }
-    ],
-    "relationships": [
-        {
-            "name": "project",
-            "type": "ToOne",
-            "target_entity": "cz.cvut.fel.nutforms.example.model.Project"
-        }
-    ]
-};
-let entityName = "cz.cvut.fel.nutforms.example.model.Bug";
-let data = {
-    "description": "Lorem Ipsum",
-    "log": "Lorem Ipsum",
-    "id": 1,
-    "localizedDescription": "Lorem Ipsum",
-    "project": 1
-};
 let contextLocation = "account";
+let bindElementId = 'form';
 
-(function (entity, contextPath) {
+// TODO: move this to a library function
+((className, contextLocation, bindElementId, entityId) => {
 
-    let modelFactory = new ModelFactory();
-    let model = modelFactory.create(className, id, metadata, data);
-    React.render(
-        <EntityForm model={model}/>,
-        document.getElementById('form')
-    );
-})(entityName, contextLocation);
+    let apiHandler = new ApiHandler('http://localhost:8080/', 'admin', '1234');
+    apiHandler.fetchMetadataFor(className).then((results) => {
+        let metadata = results[0];
+        apiHandler.fetchDataFor(className, entityId).then((data) => {
+            let modelFactory = new ModelFactory();
+            let model = modelFactory.create(className, entityId, metadata, data);
+            React.render(
+                <EntityForm model={model}/>,
+                document.getElementById(bindElementId)
+            );
+        });
+    });
+})(className, contextLocation, bindElementId, id);
