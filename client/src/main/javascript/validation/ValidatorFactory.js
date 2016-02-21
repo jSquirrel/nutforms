a07aa5c3bc9282dad8f93aa49b8d062f661868d3
Ondra Kratochvíl <ondrej.kratochvil@keyup.eu>
2016-02-21 17:40:43 +0100

    Add regex rule support
    Refs #32

--- client/src/main/javascript/validation/ValidatorFactory.js
+++ client/src/main/javascript/validation/ValidatorFactory.js
@@ -34,10 +34,12 @@ export default class ValidatorFactory {
             // toDo: implement function creation for multiple constraints and multiple fields
             // get the first word, i.e. sequence of letters separated by non-letter
             let field = this.getField(rule.condition);
+            var that = this;
             if (field !== null) {
                 return function (args) {
                     // cannot declare with 'let' keyword, otherwise the variable in anonymous function would evaluate as false
-                    var evalResult = eval('var ' + field + '="' + args.value + '";' + rule.condition);
+                    console.log('var ' + field + '="' + args.value + '";' + that.rewriteCondition(rule.condition));
+                    var evalResult = eval('var ' + field + '="' + args.value + '";' + that.rewriteCondition(rule.condition));
                     let url = document.location.origin + '/';
                     let apiHandler = new ApiHandler(url, 'admin', '1234');
                     apiHandler.fetchLocalization(locale, `rule/${rule.pckg}`).then((data) => {
@@ -58,9 +60,38 @@ export default class ValidatorFactory {
     /**
      * Returns field name, to which the function should be bound
      *
-     * @param expression
+     * @param {string} expression
+     * @returns {?string} field name
      */
     static getField(expression) {
-        return /[a-zA-Z]+/.exec(expression);
+        let fieldName = /[a-zA-Z]+/.exec(expression);
+        return fieldName === null && fieldName.length > 0 ? null : fieldName[0];
+    }
+
+    /**
+     * Rewrites the raw Drools rule condition to a form where it can be evaluated with JavaScript
+     *
+     * @param {string} condition
+     * @returns {string} rewritten condition that can be safely passed to <code>eval()</code> JS function
+     */
+    static rewriteCondition(condition) {
+        let rewritten = condition;
+        // rewrite regex matching
+        let split = rewritten.split(' ');
+        while (split.indexOf('~=') > -1) {  // matches gets rewritten to '~=' in Drools
+            let matchesIndex = split.indexOf('~=');
+            let regex = split[matchesIndex + 1];
+            var regexIndex = matchesIndex + 1;
+            while (regex.length > 1 && regex.charAt(regex.length - 1) !== '"') {    // fix regex containing spaces
+                regex += ' ' + split[++regexIndex];
+            }
+            split.length = 3;
+            split[matchesIndex + 1] = split[matchesIndex - 1] + ')';    // matches the opening bracket of 'test('
+            split[matchesIndex - 1] = regex.replace(/"/g,'/');  // globally replace quotes by slashes (RegExp notation)
+            split[matchesIndex] = '.test(';
+            rewritten = split.join(' ');
+        }
+        console.log(rewritten);
+        return rewritten;
     }
 }
