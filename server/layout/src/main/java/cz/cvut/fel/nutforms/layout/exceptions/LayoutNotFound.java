35476d08f77ed20b240beb0d97720cab178cf62a
Filip Klimes <filip@filipklimes.cz>
2016-02-25 11:24:06 +0100

    Added Layout module + LayoutServlet config to example app

---
+++ server/layout/src/main/java/cz/cvut/fel/nutforms/layout/exceptions/LayoutNotFound.java
@@ -0,0 +1,10 @@
+package cz.cvut.fel.nutforms.layout.exceptions;
+
+/**
+ * @author klimesf
+ */
+public class LayoutNotFound extends Throwable {
+    public LayoutNotFound(String message) {
+        super(message);
+    }
+}
