9f86dddcc16ec4e245fd12e084160564501ea245
Filip Klimes <filip@filipklimes.cz>
2016-02-25 13:05:03 +0100

    Added WidgetServlet + WidgetFinder

---
+++ server/widget/src/main/java/cz/cvut/fel/nutforms/widget/exceptions/WidgetNotFound.java
@@ -0,0 +1,10 @@
+package cz.cvut.fel.nutforms.widget.exceptions;
+
+/**
+ * @author klimesf
+ */
+public class WidgetNotFound extends Throwable {
+    public WidgetNotFound(String message) {
+        super(message);
+    }
+}
